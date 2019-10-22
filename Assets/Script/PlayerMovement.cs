b8e6b2680ba86741458672eedbc5e6f34a0c1d87
AlexTataru <gh0stzzzgh0st@gmail.com>
2019-10-22 18:20:05 +0300

    Fix update
    Script comments changed to english.
    Removed System.Collections or System.Collections.Generic from script in order for the script to be cleaner and not to create confusion.
    Changed RigidBody2D to RigidBody2D ball in script in order to be more meaningfull and not to create confusion.
    Removed commets from start() and update() function in script in order for the code to be cleaner.
    Created seperate folders for each type of files.

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed; //variable for ball speed
+    private float moveInput; // detects left or right key when pressed
+    private  Rigidbody2D ball; // variable for rigid body (private to only acces it from the script)
+
+   
+    
+    void Start()
+    {
+        ball = GetComponent<Rigidbody2D>(); // acces rigid body from script
+    }
+
+   
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal"); // if i press right key moveInput = 1,left key = -1 
+        ball.velocity = new Vector2(moveInput * speed, ball.velocity.y); // adds force to the ball movement in order for the player to move,doesn't affect y axis
+    }
+}
